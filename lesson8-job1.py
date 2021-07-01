6283887f730c509cbfb7c2f0c451f94505af0d61
gi.mr@yandex.ru <>
2021-07-01 18:41:01 +0300

    Думаю что выполнил задание №3 урок 8

--- lesson8-job1.py
+++ lesson8-job1.py
@@ -1,5 +1,4 @@
-
-
+import os
 #Задание №1
 #Читая файл, каждую строчку делает элементом списка даже пустую, по пустой строчке можно и отсортировать 
 lines = []
@@ -36,10 +35,9 @@ for big_key in cook_book.keys():
         dict_after_split = {}
     cook_book[big_key] = with_split    
 
-#print(cook_book)
-
-
-
+print(f'Задание №1')
+print(cook_book)
+print()
 
 #Задание №2 
 #Нужно написать функцию, которая на вход принимает список блюд из cook_book и количество персон для кого мы будем готовить
@@ -52,25 +50,43 @@ def get_shop_list_by_dishes(dishes, person_count):
 {'measure': cook_book[x][x2]['measure'], 'quantity': (int(cook_book[x][x2]['quantity']) * int(person_count))}
     print(new_dict_for_menu)
 
+print(f'Задание №2')
 get_shop_list_by_dishes(['Фахитос', 'Омлет'], 2)
+print()
 
+#Задание №3 
+#В папке ./lesson8/ лежит некоторое количество файлов. Считайте, что их количество и имена вам заранее известны
+#Необходимо объединить их в один по следующим правилам:
+#1. Содержимое исходных файлов в результирующем файле должно быть отсортировано по количеству строк в них (то есть первым нужно записать файл с наименьшим количеством строк, а последним - с наибольшим)
+#2. Содержимое файла должно предваряться служебной информацией на 2-х строках: имя файла и количество строк в нем
 
+print()
+print(f'Задание №3')
+#Получение списка заполненого полными путями к файлам (правда тут он относительный потому что задан из исходного каталога ./lesson8/)
+directory_for_file = "./lesson8/"
+tree = os.walk(directory_for_file)
+path_files = []                          # Список с полными относительными путями 
+for d, dirs, files in tree:
+    for x in files:
+        path = os.path.join(d,x)         #Формирование полного пути к файлу
+        path_files.append(path)          #Добавление строки полного пути в список 
+dir_file = d                             #Сохраняет путь к директории для создания файла 
 
+#Подсчет строчек в файле и создание словаря ключ - путь к фалу, значение - количество строк
+data_about_path_files = {}                 
+for one_path_file in path_files:
+    with open(one_path_file) as file:
+        line_count = 0
+        for line in file:
+            line_count += 1
+    data_about_path_files[line_count] = one_path_file
 
+#Создаем файл, сортируем словарик по ключам, после записываем все в созданный файл начиная с файлов где меньше всего ключ(он же и количество строчек в файле)   
+with open(f'{dir_file}/test.txt', 'w') as my_file:
+    for x in sorted(data_about_path_files.keys()):  
+        list_for_file = [f'Название файла: {data_about_path_files[x]} ',\
+f'Число строк: {x}', f'Сам текст: \n {open(data_about_path_files[x]).read()}']
+        for line in list_for_file:
+            my_file.write(line + '\n')
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print(f'Создан файл {dir_file}test.txt')
