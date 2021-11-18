7bfafa52db2af4566244943ec719c3890719fb23
gi.mr@yandex.ru <>
2021-11-18 23:01:42 +0300

    Работа с регулярками

--- Курс_2_Профессиональная_работа_с_Python/Лекция_3_Веб-скрапинг/web-scrapping.py
+++ Курс_2_Профессиональная_работа_с_Python/Лекция_3_Веб-скрапинг/web-scrapping.py
@@ -1,14 +1,172 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+import requests
+from bs4 import BeautifulSoup
 
-driver = webdriver.Firefox()
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element_by_name("q")
-elem.send_keys("A")
-elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
-#driver.close()
+headers = {'User-Agent': 'Mozilla/5.0'}  #Оказывается нужно передавать user-agent для сайта Хабр ну или не только для него 
+
+ret = requests.get('https://habr.com/ru/all/', headers=headers)
+ret = ret.text
+soup = BeautifulSoup(ret, features='html.parser')
+
+#Получить всю статью с, почемуто, не полной вложенностью через web 
+articles = soup.find(id='589727')         #(class_="tm-article-snippet__title-link")#('article', id='589727') 
+#print(articles) 
+
+#Получить название статьи
+title_from_articles = articles.find(class_="tm-article-snippet__title-link")
+print(title_from_articles.text)
+
+#Получить ссылка на хабы 
+hubs_from_articles = articles.find_all('div', class_='tm-article-snippet__hubs')
+for x in hubs_from_articles:
+    print(x.text)
+
+
+
+
+
+
+
+
+
+
+# ret = requests.get('https://habr.com/ru/all/')
+# ret = ret.text
+# soup = BeautifulSoup(ret, features='html.parser')
+#print(soup)
+
+#Получить всю статью с, почемуто, не полной вложенностью
+#articles = soup.find_all('div',class_='article-formatted-body article-formatted-body_version-2') 
+#print(ret) 
+
+# #Получить название статьи
+# title_from_articles = articles.find(class_="tm-article-snippet__title-link")
+# #print(title_from_articles.text)
+
+# #Получить объект откуда брать ссылку на хабы 
+# objeckt_hubs_from_articles = articles.find('div', class_="tm-article-snippet")
+# #print(objeckt_hubs_from_articles.prettify())
+
+# #Получить ссылка на хабы 
+# hubs_from_articles = articles.find_all('div', class_='tm-article-snippet__hubs')
+# #print(hubs_from_articles)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# soup = BeautifulSoup(ret.text, features='html.parser')
+# articles = soup.find() 
+# 
+# print(articles)
+
+
+
+
+
+
+
+
+
+
+
+# article_title = articles[0].find_all(class_="tm-articles-subpage")
+#print(articles)
+# print(article_title[0].prettify())
+# print()
+# print(article_title[0].text)
+
+# article_hub = articles[0].find_all('div', class_="tm-article-snippet__hubs")
+# print(article_hub)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                         #.prettify() красивый вывод html в консоль 
+#print()
+# print(articles[0].find_all('div', class_="article-formatted-body"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from selenium import webdriver
+# from selenium.webdriver.common.keys import Keys
+
+# # определяем список ключевых слов
+# KEYWORDS = ['дизайн', 'фото', 'web', 'python']
+
+# URL = "https://habr.com/ru/all/"
+
+# driver = webdriver.Firefox()   #Нужно установить geckodriver
+# driver.get(URL)
+
+# elem = driver.find_element_by_name("article")
+# # elem.send_keys("A")
+# # elem.send_keys(Keys.RETURN)
+# #assert "No results found." not in driver.page_source
+
+# driver.close()
 
 
 
