94e3603cb8cb05144aca6410f84e5ce8a7440649
Minashvili George <gi.mr@yandex.ru>
2021-11-22 08:49:26 +0300

    Работа по web scrapping

--- Курс_2_Профессиональная_работа_с_Python/Лекция_3_Веб-скрапинг/web-scrapping.py
+++ Курс_2_Профессиональная_работа_с_Python/Лекция_3_Веб-скрапинг/web-scrapping.py
@@ -1,5 +1,5 @@
-import requests
 from bs4 import BeautifulSoup
+import requests
 
 headers = {'User-Agent': 'Mozilla/5.0'}  #Оказывается нужно передавать user-agent для сайта Хабр ну или не только для него 
 ret = requests.get('https://habr.com/ru/all/', headers=headers)
@@ -7,331 +7,29 @@ ret = ret.text
 soup = BeautifulSoup(ret, features='html.parser')
 
 #По заданию определяем список слов для поиска 
-keyworlds = ['дизайн', 'фото', 'web', 'python', 'Neovim']
+keyworlds = ['дизайн', 'фото', 'web', 'Python']
 
-#Список статеек где есть подходящие слова 
-list_article = []
+#Словарик статеек где есть подходящие слова 
+arry_article = {}
+
 #Получить все статьи по тегу article, статеек обычно 20 штук
 text_for_get_id = soup.find_all('article')
 for one_article in text_for_get_id:
-    #print(one_article.text)
     for one_word_from_list in keyworlds:
         if one_word_from_list in one_article.text:
-            #Найденое слово
-            print(one_word_from_list)
             #Получить <Дату>
-            date_from_articles = one_article.find('span', class_="tm-article-snippet__datetime-published")
-            print(date_from_articles.find('time').attrs['title'])
+            date_from_articles = one_article.find('span', class_="tm-article-snippet__datetime-published").find('time').attrs['title']
+            
             #Получить <заголовок> 
-            title_from_articles = one_article.find(class_="tm-article-snippet__title-link")
-            print(title_from_articles.text)
+            title_from_articles = one_article.find(class_="tm-article-snippet__title-link").text
+            
             #Получить <Ссылку> 
-            #ДОПИСАТЬ ПОЛУЧЕНИЕ ССЫЛКИ И ЗАПОЛНЕНИЕ СПИСКА ИЛИ СЛОВАРЯ ДЛЯ КАЖДОЙ СТАТЬИ 
-            #СЛОВАРЬ КЛЮЧ = Название статьи ЗНАЧЕНИЕ = список из всего остального 
-
-
-
-
-
-
-
-
-
-
-
-
-    #print('xxxxxxxxxxxxxxxxxxxxxxxx')
-    #print('xxxxxxxxxxxxxxxxxxxxxxxx')
-
-
-
-
-# mylist = ['abc123', 'def456', 'ghi789', 'ABC987', 'aBc654']
-# sub = 'abc'
-
-# print "\n".join(s for s in mylist if sub.lower() in s.lower())
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# если в тексте есть слово из списка то сохранить статью с список 
-# если нет проверить следующею статью и следующее слово 
-
-
-
-
-# #Получиь] всю статью с, почемуто, не полной вложенностью через web 
-# articles = soup.find(id='590237')         #(class_="tm-article-snippet__title-link")#('article', id='589727') 
-# #print(articles) 
-
-# #Получить название статьи
-# title_from_articles = articles.find(class_="tm-article-snippet__title-link")
-# print(title_from_articles.text)
-
-# #Получить ссылка на хабы 
-# hubs_from_articles = articles.find_all('div', class_='tm-article-snippet__hubs')
-# for x in hubs_from_articles:
-#     print(x.text)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# ret = requests.get('https://habr.com/ru/all/')
-# ret = ret.text
-# soup = BeautifulSoup(ret, features='html.parser')
-#print(soup)
-
-#Получить всю статью с, почемуто, не полной вложенностью
-#articles = soup.find_all('div',class_='article-formatted-body article-formatted-body_version-2') 
-#print(ret) 
-
-# #Получить название статьи
-# title_from_articles = articles.find(class_="tm-article-snippet__title-link")
-# #print(title_from_articles.text)
-
-# #Получить объект откуда брать ссылку на хабы 
-# objeckt_hubs_from_articles = articles.find('div', class_="tm-article-snippet")
-# #print(objeckt_hubs_from_articles.prettify())
-
-# #Получить ссылка на хабы 
-# hubs_from_articles = articles.find_all('div', class_='tm-article-snippet__hubs')
-# #print(hubs_from_articles)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# soup = BeautifulSoup(ret.text, features='html.parser')
-# articles = soup.find() 
-# 
-# print(articles)
-
-
-
-
-
-
-
-
-
-
-
-# article_title = articles[0].find_all(class_="tm-articles-subpage")
-#print(articles)
-# print(article_title[0].prettify())
-# print()
-# print(article_title[0].text)
-
-# article_hub = articles[0].find_all('div', class_="tm-article-snippet__hubs")
-# print(article_hub)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                                         #.prettify() красивый вывод html в консоль 
-#print()
-# print(articles[0].find_all('div', class_="article-formatted-body"))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# from selenium import webdriver
-# from selenium.webdriver.common.keys import Keys
-
-# # определяем список ключевых слов
-# KEYWORDS = ['дизайн', 'фото', 'web', 'python']
-
-# URL = "https://habr.com/ru/all/"
-
-# driver = webdriver.Firefox()   #Нужно установить geckodriver
-# driver.get(URL)
-
-# elem = driver.find_element_by_name("article")
-# # elem.send_keys("A")
-# # elem.send_keys(Keys.RETURN)
-# #assert "No results found." not in driver.page_source
-
-# driver.close()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#import requests
-#from bs4 import BeautifulSoup
-#
-#ret = requests.get('https://2ip.ru/')
-##print(ret.text)
-#soup = BeautifulSoup(ret.text, 'html.parser')
-#el = soup.find(id='d_clip_button')
-#ip = el.text
-#print(ip)
-#
-
-
-
-
-#import requests
-# ret = requests.get('https://2ip.ru/')
-# #print(ret.text)
-
-# id_pos = ret.text.find('id="d_clip_button"')
-# ip_start_pos = ret.text.find('<span>', id_pos) + 6
-# ip_end_pos = ret.text.find('</', ip_start_pos)
-# ip = ret.text[ip_start_pos:ip_end_pos]
-
-# print(ip)
-
-
+            href_from_article = one_article.find('h2', class_="tm-article-snippet__title tm-article-snippet__title_h2").find('a', class_="tm-article-snippet__title-link").attrs['href']
+            href_from_article = f'https://habr.com{href_from_article}'
+            
+            #Составляем словарик по заданию "список подходящих статей в формате: <дата> - <заголовок> - <ссылка>"
+            arry_article[date_from_articles] = [title_from_articles, href_from_article]
+            
+#Вывод и завершение задания             
+print(arry_article)
 
